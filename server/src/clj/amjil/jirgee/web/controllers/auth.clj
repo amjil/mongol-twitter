39a8493717fc43af54ab747b937eaed948d5b2c9
amjil <amjil@protonmail.com>
2023-07-14 21:45:29 +0800

    login api

--- server/src/clj/amjil/jirgee/web/controllers/auth.clj
+++ server/src/clj/amjil/jirgee/web/controllers/auth.clj
@@ -1,19 +1,20 @@
 (ns amjil.jirgee.web.controllers.auth
   (:require
    [ring.util.http-response :as http-response]
-   
+
    [amjil.jirgee.web.utils.db :as db]
    [amjil.jirgee.web.utils.check :as check]
-   [amjil.jirgee.web.utils.token :as token]
-   ))
+   [amjil.jirgee.web.utils.token :as token]))
 
 (declare check-password)
 
 (defn login
   [conn secret params]
-  (let [entity (db/find-one-by-keys conn :users (dissoc params :password :token :code))]
+  (let [entity (db/find-one-by-keys conn :accounts (dissoc params :password :token :code))]
     (-> entity
         (check/check-must-exist "User must exists!")
-        (check/check-locked "User locked!"))
-        (check/check-password params "User Password Do Not Match!")
+        (check/check-locked "User locked!")
+        (check/check-password params "User Password Do Not Match!"))
     {:token (token/jwt-token secret (:id entity))}))
+
+
--- server/src/clj/amjil/jirgee/web/utils/check.clj
+++ server/src/clj/amjil/jirgee/web/utils/check.clj
@@ -2,23 +2,27 @@
   (:require 
    [buddy.hashers :as hashers]))
 
-(derive :system.exception/business ::user-not-found)
+(derive :system.exception/not-found ::user-not-found)
 (derive :system.exception/business ::user-already-exists)
 (derive :system.exception/business ::user-locked)
 (derive :system.exception/business ::user-password-do-not-match)
 
 (defn check-must-exist [entity info]
   (when (empty? entity)
-    (throw (ex-info info {:type ::user-not-found}))))
+    (throw (ex-info info {:type ::user-not-found
+                          :message info}))))
 
 (defn check-must-not-exist [entity info]
   (when-not (empty? entity)
-    (throw (ex-info info {:type ::user-already-exists}))))
+    (throw (ex-info info {:type ::user-already-exists
+                          :message info}))))
 
 (defn check-locked [entity info]
   (when (:locked_at entity)
-    (throw (ex-info info {:type ::user-locked}))))
+    (throw (ex-info info {:type ::user-locked
+                          :message info}))))
 
 (defn check-password [entity params info]
   (when-not (hashers/check (:password params) (:encrypted_password entity))
-    (throw (ex-info info {:type ::user-password-do-not-match}))))
+    (throw (ex-info info {:type ::user-password-do-not-match
+                          :message info}))))
