a652d03135dae3f3569e403c3a1267c995a8348e
amjil <amjil@protonmail.com>
2023-07-28 17:23:57 +0800

    tweet add link

--- server/resources/migrations/20230718120112-add-tweet-entities-table.up.sql
+++ server/resources/migrations/20230718120112-add-tweet-entities-table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS tweet_entities (
     "tweet_id" uuid REFERENCES tweets ON DELETE CASCADE,
     "media_links" text[] CHECK (array_length("media_links", 1) <= 6),
+    "link" text,
     "created_at" timestamp without time zone DEFAULT CURRENT_TIMESTAMP
 );
